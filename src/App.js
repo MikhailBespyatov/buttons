e612b37c389e60521b03b4b5d8635398d899be6f
Misha <besp9tov.mikhail94@gmail.com>
2021-08-28 21:27:28 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
+import { useState } from "react";
+import { Button } from "@material-ui/core";
 
-function App() {
+export default function App() {
+  const [value, setValue] = useState(1);
+  const [change, setChange] = useState(false);
+
+  const onMainButton = evt => {
+    evt.preventDefault();
+    setChange(true);
+  };
+
+  const onChangeButtons = evt => {
+    evt.preventDefault();
+    setChange(false);
+    setValue(evt.target.textContent);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {change
+        ? <div>
+            <Button
+              variant="contained"
+              color="primary"
+              style={{ marginRight: "10px" }}
+              onClick={onChangeButtons}
+            >
+              1
+            </Button>
+            <Button
+              variant="contained"
+              color="primary"
+              style={{ marginRight: "10px" }}
+              onClick={onChangeButtons}
+            >
+              2
+            </Button>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={onChangeButtons}
+            >
+              3
+            </Button>
+          </div>
+        : <Button variant="contained" color="primary" onClick={onMainButton}>
+            {value}
+          </Button>}
     </div>
   );
 }
-
-export default App;
